938152a99545c849b3c9120f80a39917bdb7da60
dandytron <104713695+dandytron@users.noreply.github.com>
2025-02-05 00:12:48 +0800

    G: Added post scraping and saving functionality.

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -27,3 +27,18 @@ SELECT feeds.name AS name, feeds.url AS url, users.name AS user
 FROM feeds
 INNER JOIN users
 ON feeds.user_id = users.id;
+
+-- name: MarkFeedFetched :one
+
+UPDATE feeds
+SET last_fetched_at = NOW(),
+updated_at = NOW()
+WHERE feeds.id = $1
+RETURNING *;
+
+-- name: GetNextFeedToFetch :one
+
+SELECT *
+FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
